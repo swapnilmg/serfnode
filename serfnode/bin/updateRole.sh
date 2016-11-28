0771b50df89095c99082027d8ecc382c1312199f
Swapnil Gaikwad <swapnilgaikwad@Swapnils-MacBook-Air.local>
2016-11-27 20:35:44 -0800

    failover for more than 2 nodes added

--- serfnode/bin/updateRole.sh
+++ serfnode/bin/updateRole.sh
@@ -3,4 +3,13 @@
 SERF_HOME=/usr/local/serfnode
 SERF_BIN=$SERF_HOME/bin/serf
 
-$SERF_BIN tags -set role=primary
+$SERF_BIN members > /tmp/members.txt
+sort -n /tmp/members.txt > /tmp/sorted-members.txt
+while read p; do
+    if [ "alive" = "`echo $p | awk {'print $3'}`" ]; then
+        if [ "`hostname`" = "`echo $p | awk {'print $1'}`" ]; then
+        	$SERF_BIN tags -set role=primary
+        fi;
+        break;
+    fi;
+done </tmp/sorted-members.txt
